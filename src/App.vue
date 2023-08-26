9372604dfe275712bd93f97e6be2d3c54f526db2
Rhuan Guilherme <rhuan23032004@gmail.com>
2023-08-26 20:39:20 -0300

    :tada: Inicio do projeto

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+
+onMounted(() => {
+    initFlowbite();
+})
+</script>
+
+<template>
+  
+  <RouterView />
+</template>
+
---
+++ src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<script setup>
+ import Lateral from '@/components/loginPage/LateralArea.vue'
+ import Login from '@/components/loginPage/LoginArea.vue'
+</script>
+
+<template>
+    <div class="w-full h-full bg-black">
+        <Lateral />
+        <Login />
+    </div>
+  
+</template>
+
+<style scoped>
+
+</style>
